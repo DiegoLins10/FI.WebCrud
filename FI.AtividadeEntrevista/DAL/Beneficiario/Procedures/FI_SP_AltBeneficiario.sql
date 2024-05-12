51327c7e2faef35990091e06103aba526f8bddaa
diegolins10 <dede140126>
2024-05-11 21:40:38 -0300

    Acesso de dados tabela Beneficiario

---
+++ FI.AtividadeEntrevista/DAL/Beneficiario/Procedures/FI_SP_AltBeneficiario.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROC FI_SP_AltBeneficiario
+     @NOME          VARCHAR (MAX) ,
+	 @CPF           VARCHAR (11) ,
+	 @IDCLIENTE     BIGINT,
+	 @Id            BIGINT
+AS
+BEGIN
+	UPDATE BENEFICIARIOS 
+	SET 
+		NOME = @NOME, 
+		CPF = @CPF, 
+		IDCLIENTE = @IDCLIENTE
+	WHERE Id = @Id
+END
